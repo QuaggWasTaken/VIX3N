bd5a837da204ac2db9e3fe0bc2f7b1e92cab711d
QuaggWasTaken <forrestfaulkner1@gmail.com>
2023-06-15 11:38:38 -0500

    Add project files.

---
+++ VIX3N_CLI/Commands.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIX3N_CLI
+{
+    internal class Commands
+    {
+        public async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            switch (command.Data.Name)
+            {
+                case "list-roles":
+                    await HandleListRoleCommand(command);
+                    break;
+            }
+        }
+        public async Task HandleListRoleCommand(SocketSlashCommand command)
+        {
+            var guildUser = (SocketGuildUser)command.Data.Options.First().Value;
+            var roleList = string.Join(",\n", guildUser.Roles.Where(x => !x.IsEveryone).Select(x => x.Mention));
+
+            var embedBuilder = new EmbedBuilder()
+                .WithAuthor(guildUser.ToString(), guildUser.GetAvatarUrl() ?? guildUser.GetDefaultAvatarUrl())
+                .WithTitle("Roles")
+                .WithDescription(roleList)
+                .WithColor(Color.Green)
+                .WithCurrentTimestamp();
+
+            await command.RespondAsync(embed: embedBuilder.Build());
+        }
+    }
+}
---
+++ VIX3N_CLI/Program.cs
@@ -0,0 +1,78 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Net;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System.Reflection;
+
+namespace VIX3N_CLI
+{
+    internal class Program
+    {
+        private DiscordSocketClient _client;
+        private ulong guildId = 562973571094282247;
+        private Commands _commands = new Commands();
+
+        public static Task Main(string[] args) => new Program().MainAsync(args);
+
+        public async Task MainAsync(string[] args)
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            _client.Ready += Client_Ready;
+
+            _client.SlashCommandExecuted += _commands.SlashCommandHandler;
+
+
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter your bot token as an argument.");
+                return;
+            }
+
+            var token = args[0];
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg);
+            return Task.CompletedTask;
+        }
+        public async Task Client_Ready()
+        {
+            var guild = _client.GetGuild(guildId);
+
+            var globalCommand = new SlashCommandBuilder();
+            globalCommand.WithName("first-global-command");
+            globalCommand.WithDescription("babys first global slash command");
+
+            var guildCommand = new Discord.SlashCommandBuilder()
+                .WithName("list-roles")
+                .WithDescription("Lists all roles of a user.")
+                .AddOption("user", ApplicationCommandOptionType.User, "The users whos roles you want to be listed", isRequired: true);
+
+            try
+            {
+                await guild.CreateApplicationCommandAsync(guildCommand.Build());
+                await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+            }
+            catch (HttpException e)
+            {
+                var json = JsonConvert.SerializeObject(e.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+
+        
+    }
+
+    
+}
